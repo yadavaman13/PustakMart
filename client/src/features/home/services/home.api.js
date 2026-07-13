1aa7beda9a182e9b3e1477c1522bbe35ba20fee8
yadavaman13 <yadavaman8511005211@gmail.com>
2026-07-13 10:36:42 +0530

    feat(home): add getListingDetailsApi and getSellerReviewsApi

--- client/src/features/home/services/home.api.js
+++ client/src/features/home/services/home.api.js
@@ -14,3 +14,15 @@ export const getHomeListingsApi = async (params = {}) => {
   const response = await api.get("/book", { params });
   return response.data;
 };
+
+// Get detailed info for a single book/bundle listing
+export const getListingDetailsApi = async (id) => {
+  const response = await api.get(`/book/${id}`);
+  return response.data;
+};
+
+// Get reviews of a specific seller
+export const getSellerReviewsApi = async (sellerId) => {
+  const response = await api.get(`/reviews/${sellerId}`);
+  return response.data;
+};
